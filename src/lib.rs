70c6175cd35a171068eebb4b78ed9e3b8b4fe54b
Guilherme de Lyra <guilyra12@gmail.com>
2020-07-08 18:05:13 -0300

    "optmizing" code using double buffers [but got worse results]

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::fmt;
 extern crate fixedbitset;
 use wasm_bindgen::prelude::*;
 extern crate js_sys;
-//use rand::Rng;
+use rand::Rng;
 
 use fixedbitset::FixedBitSet;
 extern crate web_sys;
@@ -39,27 +39,28 @@ impl<'a> Drop for Timer<'a> {
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
+// #[wasm_bindgen]
 pub struct Universe {
     size: usize,
     width: u32,
     height: u32,
-    cells: FixedBitSet,
+    cells: Vec<FixedBitSet>,
+    buffer: usize,
 }
 
-#[wasm_bindgen]
+// #[wasm_bindgen]
 impl Universe {
     pub fn new() -> Universe {
         utils::set_panic_hook();
-        // let mut rng = rand::thread_rng();  // for benches
+        let mut rng = rand::thread_rng(); // for benches
 
         let width = 120;
         let height = 120;
         let size = (width * height) as usize;
-        let mut cells = FixedBitSet::with_capacity(size);
-
+        let mut cells = vec![FixedBitSet::with_capacity(size); 2];
+        let buffer = 0;
         for i in 0..size {
-            cells.set(i, js_sys::Math::random() < 0.5); //rng.gen_range(0.0, 1.0) < 0.5);
+            cells[buffer].set(i, rng.gen_range(0.0, 1.0) < 0.5); // js_sys::Math::random() < 0.5);
         }
 
         Universe {
@@ -67,24 +68,25 @@ impl Universe {
             width,
             height,
             cells,
+            buffer,
         }
     }
 
     pub fn set_alive(&mut self, row: u32, col: u32) {
         let idx = self.get_index(row % self.height(), col % self.width());
-        self.cells.set(idx, true);
+        self.cells[self.buffer].set(idx, true);
     }
 
     pub fn clear(&mut self) {
         for i in 0..self.size {
-            self.cells.set(i, false);
+            self.cells[self.buffer].set(i, false);
         }
     }
 
     pub fn reset(&mut self) {
-        // let mut rng = rand::thread_rng();  // for benches
+        let mut rng = rand::thread_rng(); // for benches
         for i in 0..self.size {
-            self.cells.set(i, js_sys::Math::random() < 0.5); //rng.gen_range(0.0, 1.0) < 0.5);
+            self.cells[self.buffer].set(i, rng.gen_range(0.0, 1.0) < 0.5); // js_sys::Math::random() < 0.5);
         }
     }
     /// Set the width of the universe.
@@ -92,7 +94,8 @@ impl Universe {
     /// Resets all cells to the dead state.
     pub fn set_width(&mut self, width: u32) {
         self.width = width;
-        self.cells.clear();
+        self.cells[0].clear();
+        self.cells[1].clear();
     }
 
     /// Set the height of the universe.
@@ -100,7 +103,8 @@ impl Universe {
     /// Resets all cells to the dead state.
     pub fn set_height(&mut self, height: u32) {
         self.height = height;
-        self.cells.clear();
+        self.cells[0].clear();
+        self.cells[1].clear();
     }
 
     pub fn width(&self) -> u32 {
@@ -112,7 +116,7 @@ impl Universe {
     }
 
     pub fn cells(&self) -> *const u32 {
-        self.cells.as_slice().as_ptr()
+        self.cells[self.buffer].as_slice().as_ptr()
     }
     pub fn render(&self) -> String {
         self.to_string()
@@ -142,96 +146,86 @@ impl Universe {
         };
 
         let nw = self.get_index(north, west);
-        count += self.cells[nw] as u8;
+        count += self.cells[self.buffer][nw] as u8;
 
         let n = self.get_index(north, column);
-        count += self.cells[n] as u8;
+        count += self.cells[self.buffer][n] as u8;
 
         let ne = self.get_index(north, east);
-        count += self.cells[ne] as u8;
+        count += self.cells[self.buffer][ne] as u8;
 
         let w = self.get_index(row, west);
-        count += self.cells[w] as u8;
+        count += self.cells[self.buffer][w] as u8;
 
         let e = self.get_index(row, east);
-        count += self.cells[e] as u8;
+        count += self.cells[self.buffer][e] as u8;
 
         let sw = self.get_index(south, west);
-        count += self.cells[sw] as u8;
+        count += self.cells[self.buffer][sw] as u8;
 
         let s = self.get_index(south, column);
-        count += self.cells[s] as u8;
+        count += self.cells[self.buffer][s] as u8;
 
         let se = self.get_index(south, east);
-        count += self.cells[se] as u8;
+        count += self.cells[self.buffer][se] as u8;
 
         count
     }
     pub fn toggle_cell(&mut self, row: u32, column: u32) {
         let idx = self.get_index(row, column);
-        self.cells.set(idx, !self.cells[idx]);
+        let cell = !self.cells[self.buffer][idx];
+        self.cells[self.buffer].set(idx, cell);
     }
 
     pub fn tick(&mut self) {
-        let _timer = Timer::new("Universe::tick");
+        // let _timer = Timer::new("Universe::tick");
+        let next_buffer = 1 - self.buffer;
 
-        // let mut next = self.cells.clone();
-        let mut revived = Vec::new();
-        let mut died = Vec::new();
-        let mut next = {
-            let _timer = Timer::new("allocate next cells");
-            self.cells.clone()
-        };
+        // let mut next = self.cells[self.buffer].clone();
+        // let mut revived = Vec::new();
+        // let mut died = Vec::new();
+        // let mut next = {
+        // //    let _timer = Timer::new("allocate next cells");
+        //     self.cells[self.buffer].clone()
+        // };
 
-        {
-            let _timer = Timer::new("new generation");
-            for row in 0..self.height {
-                for col in 0..self.width {
-                    let idx = self.get_index(row, col);
-                    let cell = self.cells[idx];
-                    let live_neighbors = self.live_neighbor_count(row, col);
+        // let _timer = Timer::new("new generation");
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[self.buffer][idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
 
-                    // log!(
-                    //     "cell[{}, {}] is initially {:?} and has {} live neighbors",
-                    //     row,
-                    //     col,
-                    //     cell,
-                    //     live_neighbors
-                    // );
+                self.cells[next_buffer].set(
+                    idx,
+                    match (cell, live_neighbors) {
+                        (true, x) if x < 2 || x > 3 => false,
+                        (true, 2..=3) => true,
+                        (false, 3) => true,
+                        (otherwise, _) => otherwise,
+                    },
+                );
 
-                    next.set(
-                        idx,
-                        match (cell, live_neighbors) {
-                            (true, x) if x < 2 || x > 3 => false,
-                            (true, 2..=3) => true,
-                            (false, 3) => true,
-                            (otherwise, _) => otherwise,
-                        },
-                    );
-
-                    // log!("    it becomes {:?}", next[idx]);
-
-                    if next[idx] != cell {
-                        if cell == true {
-                            died.push(idx);
-                        } else {
-                            revived.push(idx);
-                        }
-                    }
-                }
+                // if self.cells[next_buffer][idx] != cell {
+                //     if cell == true {
+                //         died.push(idx);
+                //     } else {
+                //         revived.push(idx);
+                //     }
+                // }
             }
         }
-        // log!("    cells that died -> {:?}", died);
-        // log!("    cells that revived -> {:?}", revived);
-        let _timer = Timer::new("free old cells");
-        self.cells = next;
+
+        // let _timer = Timer::new("free old cells");
+        self.buffer = next_buffer;
+        // self.cells[self.buffer] = next;
     }
 }
 
 impl Universe {
     /// Get the dead and alive values of the entire universe.
     pub fn get_cells(&self) -> &FixedBitSet {
-        &self.cells
+        &self.cells[self.buffer]
     }
 
     /// Set cells to be alive in a universe by passing the row and column
@@ -239,7 +233,7 @@ impl Universe {
     pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
         for (row, col) in cells.iter().cloned() {
             let idx = self.get_index(row, col);
-            self.cells.set(idx, true);
+            self.cells[self.buffer].set(idx, true);
         }
     }
 }
@@ -249,7 +243,7 @@ impl fmt::Display for Universe {
         for row in 0..self.height {
             for col in 0..self.width {
                 let idx = self.get_index(row, col);
-                let cell = self.cells[idx];
+                let cell = self.cells[self.buffer][idx];
                 let symbol = if cell == false { '◻' } else { '◼' };
                 write!(f, "{}", symbol)?;
             }
